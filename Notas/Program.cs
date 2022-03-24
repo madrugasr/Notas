6873ca08b09ad004889af153584ca35bd596211f
Daniel Marques <marques.daniel@outlook.com.br>
2022-03-24 14:21:26 +0000

    Update. 0.2

--- Notas/Program.cs
+++ Notas/Program.cs
@@ -19,12 +19,11 @@ static int ValidaNumero(int menor, int maior, bool msg)
         testaStr = int.TryParse(temp, out num);
 
         if (!testaStr || num < menor || num > maior)
-        {
             Console.WriteLine("Número inválido! Insira novamente!");
-        }
-        else {
+        
+        else
             ok = true;
-        }
+        
 
     } while (!ok);
 
@@ -33,7 +32,7 @@ static int ValidaNumero(int menor, int maior, bool msg)
 }
 
 //Menu de Opções.
-int Menu()
+static int Menu()
 {
     Console.Clear();
     Console.WriteLine("ARRAY de NOTAS");
@@ -44,17 +43,22 @@ int Menu()
     Console.WriteLine("5. Menor Nota");
     Console.WriteLine("6. Aprovados e Reprovados");
     Console.WriteLine("7. Adicionar Nota");
-    Console.WriteLine("8. Sair");
+    Console.WriteLine("8. Alterar Nota");
+
+    Console.WriteLine("9. Sair");
 
     //Retorna o valor do operador.
-    return Convert.ToInt32("\n" + Console.ReadLine());
+    return ValidaNumero(1, 9, true);
 }
 
-
 //Declaração de variáveis.
-int[] notas = new int[0]; //[0] ... [4]
+float[] notas = new float[0]; //[0] ... [4]
 
-int opcao;
+int opcao; 
+
+Console.WriteLine("Dimensão do Array é = {0}", notas.Length);
+Console.ReadKey();
+           
 
 //Ciclo de repetição dos Menus.
 do
@@ -69,19 +73,23 @@ do
             Console.Clear();
             Console.WriteLine("Inserindo Notas...");
 
-            Console.Write("Quantas notas: ");
-            int quantNotas = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nQuantas notas deseja adicionar? ");
+            int notasAdicionais = ValidaNumero(1, 100, false);
 
-            notas[quantNotas];
+            int tamanhoAtual = notas.Length;
 
-            for (int i = 1; i <= notas.Length; i++)
+            int novoTamanho = notasAdicionais + tamanhoAtual;
+
+            Array.Resize(ref notas, novoTamanho);
+
+            for (int i = tamanhoAtual; i < novoTamanho; i++)
             {
-                Console.Write($"\nInsira a Nota {i}: ");
-                notas[i - 1] = ValidaNumero(0, 20, false);
+                Console.WriteLine($"\nInsira as nota(as) {i}: ");
+                notas[i] = ValidaNumero(0, 20, false);
             }
+                
             Console.ReadKey();
             Console.Clear();
-
             break;
 
         //Lista o array.
@@ -90,18 +98,15 @@ do
             Console.WriteLine("Listando as Notas..");
 
             for (int i = 0; i <= notas.Length; i++)
-            {
                 Console.WriteLine(notas[i]);
-            }
+            
             Console.ReadKey();
             Console.Clear();
-
             break;
 
         //Média
         case 3:
-            int media = 0;
-            int soma = 0;
+            float media = 0, soma = 0;
 
             Console.Clear();
             Console.WriteLine("MÉDIA");
@@ -109,28 +114,29 @@ do
             for (int i = 0; i <= 4; i++)
                 soma += notas[i];
 
-            media = soma / 5;
+            media = soma / notas.Length;
             Console.WriteLine("\nMédia Notas:" + media.ToString("F2"));
 
             break;
 
         //Maior Nota
         case 4:
+
             Console.Clear();
             Console.WriteLine("MAIOR NOTA");
+            
+           float maiorNota = 0;
 
-            int maiorNota = 0;
-
-            for (int i = 0; i <= notas.Length; i++)
+           for (int i = 0; i < notas.Length; i++)
             {
-                if (i == 0)
+                if (i == 0)  
                     maiorNota = notas[i];
 
-                if (notas[i] > maiorNota)
+                if (notas[i] > maiorNota) 
                     maiorNota = notas[i];
-            }
-
-            Console.WriteLine($"Maior Nota: {maiorNota}...");
+             }
+           Console.WriteLine($"A maior nota é = {maiorNota}");
+   
             Console.ReadKey();
             Console.Clear();
 
@@ -141,19 +147,41 @@ do
             Console.Clear();
             Console.WriteLine("MENOR NOTA");
 
-            int menorNota = 0;
-
-            for (int i = 0; i <= notas.Length; i++)
+            float menorNota = 0;
+             
+            for (int i = 0; i < notas.Length; i++)
             {
-                if (i == 0)
+                if (i == 0) 
                     menorNota = notas[i];
 
                 if (notas[i] < menorNota)
                     menorNota = notas[i];
             }
-            Console.WriteLine($"Menor Nota: {menorNota}");
+            
+            Console.WriteLine($"A menor nota é = {menorNota}");
+ 
             Console.ReadKey();
             Console.Clear();
+            break;
+
+        //Aprovados e Reprovados
+        case 6:       
+            Console.WriteLine("\nAPROVADOS E REPROVADOS");
+                        
+            int numeroAprovados = 0, numeroReprovados = 0;                     
+     
+            for (int i = 0; i < notas.Length; i++)          
+            {  
+                if (notas[i] >= 10)          
+                    numeroAprovados += 1;
+                
+                if (notas[i] < 10)   
+                    numeroReprovados += 1;      
+            }
+                        
+            Console.WriteLine($"Aprovados: {numeroAprovados}");
+            Console.WriteLine($"Reprovdos: {numeroReprovados}");
+                        
             break;
 
         //Adicionando uma nota.
@@ -168,9 +196,22 @@ do
             Console.Write($"Insira a {notas.Length}: ");
             notas[notas.Length - 1] = ValidaNumero(0, 20, false);
 
+            break;
+
+        case 8: 
+            for (int i = 1; i <= notas.Length; i++)
+                Console.WriteLine($"{i} * notas = {notas[i -1 ]}");
+          
+            Console.WriteLine($"Qual o índice da nota que deseja alterar? ");
+            int indiceNota = ValidaNumero(1, notas.Length, true);
+
+            notas[indiceNota - 1] = ValidaNumero(1, 20, true);
+
+            break;
+
         default:
             System.Environment.Exit(0);
             break;
     }
 
-} while (opcao != 8);
+} while (opcao != 9);
